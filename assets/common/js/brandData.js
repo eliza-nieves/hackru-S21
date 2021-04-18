d7225ee661068ab7f6f7639af957acb2ae8481b2
bphogat1006 <b.phogat1925@gmail.com>
2021-04-18 11:10:16 -0400

    env scores & navbar

--- assets/common/js/brandData.js
+++ assets/common/js/brandData.js
@@ -1,17 +1,43 @@
 
 str = window.location.href;
-var res = str.split("brand=")[1].replace(/%20/g, " ").replace(/%27/g, "'");
-// window.prompt(res);
+var name = str.split("brand=")[1].replace(/%20/g, " ").replace(/%27/g, "'");
+// window.prompt(name);
 
-var info = search(res);
+var environmentalScore = "N/A"
+switch(name) {
+  case "pacsun":
+    environmentalScore = 445
+    break;
+  case "zara":
+    environmentalScore = 200
+    break;
+  case "hanes":
+    environmentalScore = 200
+    break;
+  case "champion":
+    environmentalScore = 210
+    break;
+  case "chanel":
+    environmentalScore = 200
+    break;
+  case "boohoo":
+    environmentalScore = 493
+    break;
+  case "uniqlo":
+    environmentalScore = 310
+    break;
+}
+
+var info = search(name);
 
 if(info.enviro_info !== "No data for given company") {
   document.getElementById("query_warning").style.display = "none"
 }
 
+
 $("#brand_title").text(info.name);
 
-$("#row1col1").text("Some number");
+$("#row1col1").text(environmentalScore);
 
 $("#row1col2").text(info.enviro_info);
 
