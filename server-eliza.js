d3afc9db0e5c6fd26d6ed67976f3a128fe0be4c5
Eliza Nieves <snowfell00@gmail.com>
2021-04-18 00:57:56 -0400

    changed server to work on my local comp

---
+++ server-eliza.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+app.get('/', function(req, res){
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/index.html');
+});
+app.get('/style.css', function(req, res){
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/style.css');
+});
+app.get('/api', function(req, res) {
+  var api_base = 'https://tf689y3hbj.execute-api.us-east-1.amazonaws.com/prod/authorization/search?';
+  var auth_token = '&token=e26c031fb362046e4232370c8cb5ae70';
+  var req_company = req.query.q;
+  fetch(api_base + 'q=' + req_company + auth_token)
+  .then(data => data.json())
+  .then(data => res.json(data))
+});
+app.listen(3000, function(){
+  console.log("Listening on port 3000!")
+});
