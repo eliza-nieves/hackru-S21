a8a76ec15242ea8ce664162735dd09821bc2ad66
Eliza Nieves <snowfell00@gmail.com>
2021-04-18 11:15:37 -0400

    functional

--- server-eliza.js
+++ server-eliza.js
@@ -6,8 +6,47 @@ app.get('/', function(req, res){
   res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/index.html');
 });
 app.get('/assets/lib/images/sweatshop.jpg', function(req, res){
+	console.log("here");
   res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/assets/lib/images/sweatshop.jpg');
 });
+
+app.get('/assets/lib/images/childlabor.jpg', function(req, res){
+	console.log("here 2");
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/assets/lib/images/childlabor.jpg');
+});
+
+app.get('/assets/common/js/parseUrl.js', function(req, res){
+	console.log("here 3");
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/assets/common/js/parseUrl.js');
+});
+
+app.get('/assets/common/html/brandinfo.html', function(req, res){
+	console.log("here 4");
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/assets/common/html/brandinfo.html');
+});
+
+app.get('/assets/common/js/data.js', function(req, res){
+	console.log("here 5");
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/assets/common/js/data.js');
+});
+
+app.get('/assets/common/js/brandData.js', function(req, res){
+	console.log("here 6");
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/assets/common/js/brandData.js');
+});
+
+app.get('/assets/common/html/brandinfo.html', function(req, res){
+	console.log("here 7");
+  res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/assets/common/html/brandinfo.html');
+});
+
+
+
+
+
+
+
+
 
 app.get('/style.css', function(req, res){
   res.sendFile('C:/Users/Eli/Documents/rutgers stuff/code/hackru21/hackru/hackru-S21/style.css');
